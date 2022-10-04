d6dea9bd3ca0c88cf0956fd3590fb2a736c8b4f2
HussinIrfan <112319824+HussinIrfan@users.noreply.github.com>
2022-10-03 21:02:57 -0700

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>     
+#include <complex> 
+#include <SFML/Graphics.hpp>
+#include "ComplexPlane.h"
+
+int main()
+{
+
+	int i;
+	return 0;
+}
